cdf1dcade197c078c9c765eb3a72203629b411a4
MartinNyberg <55439200+MartinNyberg@users.noreply.github.com>
2019-10-01 12:56:15 +0200

    2

---
+++ Prov-2019-10-01/Prov-2019-10-01/Bok.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_2019_10_01
+{
+    class Bok
+    {
+        public int price = 100;
+        private string name;
+        private int rarity;
+        private string kategori;
+        private int value;
+
+
+    }
+}
---
+++ Prov-2019-10-01/Prov-2019-10-01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_2019_10_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
